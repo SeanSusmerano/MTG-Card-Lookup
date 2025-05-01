694ae45ca1547c61d5830f2ce04796c913d6d106
Sean Susmerano <susmeranosean@gmail.com>
2025-05-01 00:18:37 -0500

    Worked on Sheet_API.py and started on main.py. Added .gitignore.

--- Scry_API.py
+++ Scry_API.py
@@ -1,3 +1,18 @@
+import requests
+
 class Scryfall_API:
     def __init__(self):
         self.placeholder = None
+
+
+    def check_Valid_Card(self, card_data:dict):
+        return (None == card_data['prices']['usd'])
+
+
+    def get_Price(self, card_data:dict):
+        return card_data['prices']['usd']
+
+
+    def request_Card_Data(self, card_name: str):
+        reachout = requests.get(f"https://api.scryfall.com/cards/named?fuzzy={card_name}")
+        return reachout.json()
