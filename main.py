8e38f1c5e1d42df2b084beac87a1acf478ac5fd5
Sean Susmerano <susmeranosean@gmail.com>
2025-05-03 17:27:04 -0500

    Completed Google Integration

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import Sheet_API, Scry_API
+import Sheet_API, Scry_API, time
 from datetime import date, datetime
 
 def check_If_Worth_Searching(card_name: str, already_searched:dict):
@@ -11,6 +11,11 @@ def check_If_Worth_Searching(card_name: str, already_searched:dict):
         return True
     else:
         return False
+    
+def wait():
+    print("Quick minute break to not exceed Google write Request Limit.")
+    time.sleep(60)
+    
 
 
 def main():
@@ -32,14 +37,23 @@ def main():
     do_Not_Search_List = document.grab_Do_Not_Search_List()
     already_Searched_Dictionary = document.grab_Already_Searched_List()
     scry = Scry_API.Scryfall_API()
+    search_Length = len(search_List)
+    counter = 0
 
     for each_card in search_List:
+        counter += 1
+        if(document.write_count > 55):
+            wait()
+            document.write_count = 0
+
         should_Check = check_If_Worth_Searching(each_card, already_Searched_Dictionary)
         if((each_card in do_Not_Search_List) or (each_card == "")):
+            print(f"Completed: {counter}/{search_Length}")
             continue
 
         card_data = scry.request_Card_Data(each_card)
         valid_card = scry.check_Valid_Card(card_data)
+        card_price = 0
         
         if(valid_card and should_Check):
             card_price = scry.get_Price(card_data)
@@ -47,14 +61,17 @@ def main():
         elif(valid_card and (each_card not in already_Searched_Dictionary)):
             card_price = scry.get_Price(card_data)
             document.Add_Already_Searched(each_card)
+        elif(not valid_card):
+            document.Add_Unable_To_Search(each_card, "Unable to find card Data.")
         else:
-            continue
+            document.Add_Unable_To_Search(each_card,"")
 
         if(float(card_price)>1.0):
             document.add_Sell_Individually(each_card,card_price)
-        
-
-
+        print(f"Completed: {counter}/{search_Length}")
+    
+    #Clear search list
+    document.Search_List_Worksheet.batch_clear(["A2:A1000"])
 
 if(__name__ == "__main__"):
     main()
