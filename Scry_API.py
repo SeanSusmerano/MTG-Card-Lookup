9f99e30a8d196e584d209a711fb557a12cd28710
Sean Susmerano <susmeranosean@gmail.com>
2025-05-25 20:17:15 -0500

    initial foil addition

--- Scry_API.py
+++ Scry_API.py
@@ -5,18 +5,29 @@ class Scryfall_API:
         self.placeholder = None
 
 
-    def check_Valid_Card(self, card_data:dict):
+    def check_Valid_Card(self, card_data:dict, card_foil:str):
+        foil_check = False
+
+        if(len(card_foil) > 0):
+            foil_check = True
+
         if('status' in card_data):
             return False
-        elif(None == card_data['prices']['usd']):
+        elif((None == card_data['prices']['usd']) and (not foil_check)):
+            return False
+        elif((None == card_data['prices']['usd_foil']) and foil_check):
             return False
         else:
             return True
     #Add a way to differentiate what the issue is, missing price, or card couldn't be found.
 
 
-    def get_Price(self, card_data:dict):
-        return card_data['prices']['usd']
+    def get_Price(self, card_data:dict, card_foil: str):
+
+        if(len(card_foil) > 0):
+            return card_data['prices']['usd_foil']
+        else:
+            return card_data['prices']['usd']
 
 
     def request_Card_Data(self, card_name: str):
--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import Sheet_API, Scry_API, time
 from datetime import date, datetime
 
-def check_If_Worth_Searching(card_name: str, already_searched:dict):
+def check_If_Worth_Searching(card_name: str, card_foil: str, already_searched:dict):
     days_since_searched = -1
+
+    if(len(card_foil) > 0):
+        card_name += " (f)"
 
     if(card_name in already_searched):
         days_since_searched = (date.today() - datetime.strptime(already_searched[card_name], '%Y-%m-%d').date()).days
@@ -33,41 +36,41 @@ def main():
             print("I'm sorry that doesn't appear to be a proper input.")
     
     document = Sheet_API.Google_API(link = user_input)
-    search_List = document.grab_Search_List()
+    search_Dict = document.grab_Search_List()
     do_Not_Search_List = document.grab_Do_Not_Search_List()
     already_Searched_Dictionary = document.grab_Already_Searched_List()
     scry = Scry_API.Scryfall_API()
-    search_Length = len(search_List)
+    search_Length = len(search_Dict)
     counter = 0
 
-    for each_card in search_List:
+    for each_card in search_Dict:
         counter += 1
         if(document.write_count > 55):
             wait()
             document.write_count = 0
 
-        should_Check = check_If_Worth_Searching(each_card, already_Searched_Dictionary)
+        should_Check = check_If_Worth_Searching(each_card, search_Dict[each_card], already_Searched_Dictionary)
         if((each_card in do_Not_Search_List) or (each_card == "")):
             print(f"Completed: {counter}/{search_Length}")
             continue
 
         card_data = scry.request_Card_Data(each_card)
-        valid_card = scry.check_Valid_Card(card_data)
+        valid_card = scry.check_Valid_Card(card_data, search_Dict[each_card])
         card_price = 0
         
         if(valid_card and should_Check):
-            card_price = scry.get_Price(card_data)
-            document.update_Already_Searched_Card(each_card)
+            card_price = scry.get_Price(card_data, search_Dict[each_card])
+            document.update_Already_Searched_Card(each_card, search_Dict[each_card])
         elif(valid_card and (each_card not in already_Searched_Dictionary)):
-            card_price = scry.get_Price(card_data)
-            document.Add_Already_Searched(each_card)
+            card_price = scry.get_Price(card_data, search_Dict[each_card])
+            document.Add_Already_Searched(each_card, search_Dict[each_card])
         elif(not valid_card):
-            document.Add_Unable_To_Search(each_card, "Unable to find card Data.")
+            document.Add_Unable_To_Search(each_card, search_Dict[each_card], "Unable to find card Data.")
         else:
-            document.Add_Unable_To_Search(each_card,"")
+            document.Add_Unable_To_Search(each_card, search_Dict[each_card], "Unable to determine issue.")
 
         if(float(card_price)>1.0):
-            document.add_Sell_Individually(each_card,card_price)
+            document.add_Sell_Individually(each_card, search_Dict[each_card], card_price)
         print(f"Completed: {counter}/{search_Length}")
     
     #Clear search list
